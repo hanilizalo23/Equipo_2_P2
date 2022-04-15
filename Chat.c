82894607b26ccf4a885dfd3ca66559568df338fc
hanilizalo23 <nelida_phm@hotmail.com>
2022-04-15 01:27:48 -0500

    Need to fill functions

--- Chat.c
+++ Chat.c
@@ -11,6 +11,7 @@
 #include "Chat.h"
 #include "stdbool.h"
 #include "fsl_uart.h"
+#include <stdio.h>
 
 #define NOTHING 0U
 #define ONE_LENGHT 1U
@@ -18,11 +19,23 @@
 
 static program_status_t g_status_chat[2];
 static uint8_t g_data_chat[2];
-
+static uint8_t g_t_connected[2];
+static message_t g_chat_message[2];
+static uint8_t g_first_in[2] = {true};
 
 //Strings for messages
 uint8_t Chat [] = "\r\nCHAT\r\n";
-
+uint8_t Message [] = "\r\n       Mensaje: ";
+uint8_t Allignment [] = "\r\n                    ";
+uint8_t Allignment0 [] ="\r\n";
+uint8_t Term1 [] = "Terminal 1:\r";
+uint8_t Term2 [] = "Terminal 2:\r";
+uint8_t Term1Disc [] = 	"\r\n-------Terminal 1 desconectada-------\r\n";
+uint8_t Term2Disc [] = 	"\r\n-------Terminal 2 desconectada-------\r\n";
+uint8_t TermWaiting [] = "\r\n       Esperando a la otra terminal\r\n";
+uint8_t Term1Con [] = 	"\r\n-------Terminal 1 conectada----------\r\n";
+uint8_t Term2Con [] = 	"\r\n-------Terminal 2 conectada----------\r\n";
+uint8_t Press_ESC1 [] = "\r\nPresione ESC para salir\r\n";
 
 //Functions pointers arrays for all the stages of each submenu
 void (*p_chat_pc_funcs[3])() = {PC_Chat_Start,PC_Chat_Transmit,PC_Chat_Exit};
