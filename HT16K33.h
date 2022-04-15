c14c106bcc2c40b127e44ec2642fbeb48bab56e9
Mauricio Peralta O <mperalta.osorio@iteso.mx>
2022-04-15 16:16:36 -0500

    HT16K33  fuctions added

--- HT16K33.h
+++ HT16K33.h
@@ -16,11 +16,15 @@
 #include "I2C.h"
 #include "Bits.h"
 
+#define LED_MATRIX_SLAVE_ADDR	(0x70U)
+#define SUB_ADDR_SIZE			(0x01U)
+#define LED_MATRIX_ADDR			(0x00)
 
 /*!
  	 \brief	 Functions to initialize LCD address, write data and make a copy of an array
  */
 uint16_t Led_Matrix_config(uint8_t SLAVE_ADDR_init);
-
+uint16_t Led_Matrix_Write(uint8_t *arr_ptr);
+void Led_Matrix_Copy_Str(uint8_t *original,uint8_t *arr_ptr, uint16_t lenght);
 
 #endif /* HT16K33_H_ */
