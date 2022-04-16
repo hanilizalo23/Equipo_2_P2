3d0eab037e0fe01c6aee33b5f906279b736d0b33
hanilizalo23 <nelida_phm@hotmail.com>
2022-04-15 20:54:38 -0500

    Works from 1-6 options, 7-8 not

--- main.c
+++ main.c
@@ -2,6 +2,36 @@
 	\file
 	\brief P2, Electronic message board
 	\author Nelida Paulina Hernández Moya y Mauricio Peralta Osorio
-	\date	04/04/2022
+	\date	14/04/2022
  */
 
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MK64F12.h"
+#include "fsl_debug_console.h"
+#include "Menus.h"
+#include "fsl_uart.h"
+
+int main(void) {
+	ConfigureAll();
+	flow_flags_t flag_flow;
+
+    while(1) {
+    	//Constantly read one character in both terminals
+    	flag_flow = ReadTerminals();
+
+    	//Do something according to the stage of the program
+    	if(flag_flow.t1_flag)
+    	{
+    		ChooseStage(PC);
+    	}
+    	if(flag_flow.t2_flag)
+    	{
+    		ChooseStage(HC05);
+    	}
+    }
+    return 0 ;
+}
